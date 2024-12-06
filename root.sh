84b10d87da8e087a9dc8fb7888c5ebc58b8ce2c5
Nguyen Truong Tan <truongtana13@gmail.com>
2024-12-06 09:03:45 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -5,3 +5,8 @@ git clone https://github.com/hctilg/root-termux.git && cd root-termux && chmod +
 pkg install wget proot -y
 echo 'bash ~/root-termux/start.sh' >> $PREFIX/etc/bash.bashrc
 bash start.sh
+
+
+Mrt 3.95 anh em tải về dùng nhé đã crack free 100% nhé anh em tải về giải nén
+pass: vietnamicfixteam
+link : https://drive.google.com/file/d/1ZLOmSiipe1JnJkSnIts5mWNK4CnGa9vc/view?usp=sharing
